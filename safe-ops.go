2ae76bbfe2e0ea0aa4fb75b652fcfeca84672b1d
Bernard Tatin <btat1.ernard@tutanota.com>
2022-06-23 16:09:00 +0200

    safe-ops.go: Some refactoring

--- safe-ops.go
+++ safe-ops.go
@@ -15,39 +15,113 @@ import (
 	"reflect"
 )
 
-func SafeAdd[V int32 | int64 | uint32 | uint64](a V, b V) (V, bool) {
-	c := a + b
-	t := reflect.TypeOf(a).Kind()
-	var good bool
-	switch t {
-	case reflect.Int32:
-		good = int32(a) <= math.MaxInt32-int32(b)
-	case reflect.Int64:
-		good = int64(a) <= math.MaxInt64-int64(b)
-	case reflect.Uint32:
-		good = uint32(a) <= math.MaxUint32-uint32(b)
-	case reflect.Uint64:
-		good = uint64(a) <= math.MaxUint64-uint64(b)
+func SSub[V int8 | int16 | int32 | int64 | uint8 | uint16 | uint32 | uint64](a V, b V) (V, bool) {
+	var isub func(a V, b V) (V, bool)
+
+	isub = func(a V, b V) (V, bool) {
+		var good bool
+		var t = reflect.TypeOf(a).Kind()
+		switch t {
+		case reflect.Int8:
+			good = int8(a) <= math.MaxInt8-int8(b)
+		case reflect.Int16:
+			good = int16(a) <= math.MaxInt16-int16(b)
+		case reflect.Int32:
+			good = int32(a) <= math.MaxInt32-int32(b)
+		case reflect.Int64:
+			good = int64(a) <= math.MaxInt64-int64(b)
+		case reflect.Uint8:
+			good = uint8(a) <= math.MaxUint8-uint8(b)
+		case reflect.Uint16:
+			good = uint16(a) <= math.MaxUint16-uint16(b)
+		case reflect.Uint32:
+			good = uint32(a) <= math.MaxUint32-uint32(b)
+		case reflect.Uint64:
+			good = uint64(a) <= math.MaxUint64-uint64(b)
+		}
+		return a + b, good
 	}
-	return c, good
+	if a == 0 {
+		return -b, true
+	} else if b == 0 {
+		return a, true
+	} else if a < b {
+		c, good := isub(b, a)
+		return -c, good
+	} else {
+		return isub(a, b)
+	}
 }
 
-func SafeMul[V int32 | int64 | uint32 | uint64](a V, b V) (V, bool) {
+func SAdd[V int8 | int16 | int32 | int64 | uint8 | uint16 | uint32 | uint64](a V, b V) (V, bool) {
+	var iadd func(a V, b V) (V, bool)
+
+	iadd = func(a V, b V) (V, bool) {
+		var good bool
+		var t = reflect.TypeOf(a).Kind()
+		switch t {
+		case reflect.Int8:
+			good = int8(a) <= math.MaxInt8-int8(b)
+		case reflect.Int16:
+			good = int16(a) <= math.MaxInt16-int16(b)
+		case reflect.Int32:
+			good = int32(a) <= math.MaxInt32-int32(b)
+		case reflect.Int64:
+			good = int64(a) <= math.MaxInt64-int64(b)
+		case reflect.Uint8:
+			good = uint8(a) <= math.MaxUint8-uint8(b)
+		case reflect.Uint16:
+			good = uint16(a) <= math.MaxUint16-uint16(b)
+		case reflect.Uint32:
+			good = uint32(a) <= math.MaxUint32-uint32(b)
+		case reflect.Uint64:
+			good = uint64(a) <= math.MaxUint64-uint64(b)
+		}
+		return a + b, good
+	}
+	if a == 0 {
+		return b, true
+	} else if b == 0 {
+		return a, true
+	} else if a > 0 && b < 0 {
+		return SSub(a, b)
+	} else if a < 0 && b > 0 {
+		return SSub(a, b)
+	} else {
+		return iadd(a, b)
+	}
+}
+
+func SMul[V int8 | int16 | int32 | int64 | uint8 | uint16 | uint32 | uint64](a V, b V) (V, bool) {
+	var imul func(a V, b V) (V, bool)
+
+	imul = func(a V, b V) (V, bool) {
+		var good bool
+		var t = reflect.TypeOf(a).Kind()
+
+		switch t {
+		case reflect.Int8:
+			good = int8(a) <= math.MaxInt8/int8(b)
+		case reflect.Int16:
+			good = int16(a) <= math.MaxInt16/int16(b)
+		case reflect.Int32:
+			good = int32(a) <= math.MaxInt32/int32(b)
+		case reflect.Int64:
+			good = int64(a) <= math.MaxInt64/int64(b)
+		case reflect.Uint8:
+			good = uint8(a) <= math.MaxUint8/uint8(b)
+		case reflect.Uint16:
+			good = uint16(a) <= math.MaxUint16/uint16(b)
+		case reflect.Uint32:
+			good = uint32(a) <= math.MaxUint32/uint32(b)
+		case reflect.Uint64:
+			good = uint64(a) <= math.MaxUint64/uint64(b)
+		}
+		return a * b, good
+	}
 	if a == 0 || b == 0 {
 		return 0, true
+	} else {
+		return imul(a, b)
 	}
-	c := a * b
-	t := reflect.TypeOf(a).Kind()
-	var good bool
-	switch t {
-	case reflect.Int32:
-		good = int32(a) <= math.MaxInt32/int32(b)
-	case reflect.Int64:
-		good = int64(a) <= math.MaxInt64/int64(b)
-	case reflect.Uint32:
-		good = uint32(a) <= math.MaxUint32/uint32(b)
-	case reflect.Uint64:
-		good = uint64(a) <= math.MaxUint64/uint64(b)
-	}
-	return c, good
 }
